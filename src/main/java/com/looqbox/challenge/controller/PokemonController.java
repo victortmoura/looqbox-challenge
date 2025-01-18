99fbb95c482957e2b08fc149f51ff1748dd9f328
Victor Moura <victorttmoura@gmail.com>
2025-01-17 23:36:21 -0300

    feat: add Controller with getPokemons endpoint

---
+++ src/main/java/com/looqbox/challenge/controller/PokemonController.java
@@ -0,0 +1,29 @@
+package com.looqbox.challenge.controller;
+
+import com.looqbox.challenge.model.PokemonResponse;
+import com.looqbox.challenge.service.PokemonService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.looqbox.challenge.sort.MergeSort.SortType;
+
+@RestController
+@RequestMapping("pokemons")
+public class PokemonController {
+
+    private PokemonService pokemonService;
+
+    public PokemonController(PokemonService pokemonService) {
+        this.pokemonService = pokemonService;
+    }
+
+    @GetMapping
+    public ResponseEntity<PokemonResponse> getPokemons(@RequestParam(required = false) String query,
+                                                       @RequestParam(required = false, defaultValue = "ALPHABETICAL") SortType sort) {
+        return ResponseEntity.ok(pokemonService.getPokemons(query, sort));
+    }
+
+}
